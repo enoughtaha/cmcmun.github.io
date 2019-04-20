171c39af7b1f50bec2bdaed4cd26ecd12edbd5fe
AT - Lab <ataha1120@gmail.com>
2019-04-20 11:13:06 -0700

    V4
    Removed prior files to V4, restarting to ensure proper standards in sizing and color properties are adopted

--- js/index.js
+++ js/index.js
@@ -2,63 +2,23 @@ $("header").load("header.html");
 $("#footer").load("footer.html");
 
 
-vh = $(window).height()/100;
-vw = $(window).width()/100;
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  $("video").attr("src","media/mobileintro.mp4");
+}
 
-(function ($) {
-  $(document).ready(function(){
-    $(function () {
-        $(window).scroll(function () {
 
-            if ($(this).scrollTop() > 30) {
-                $('header').fadeIn(300);
-            } else {
-                 $('header').fadeOut(300);
-            }
-        });
-    });
-
+$('.intro-continue').click(function() {
+    $('html,body').animate({
+            scrollTop: $('#about').offset().top},
+            'slow');
 });
-  }(jQuery));
 
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    $("video").attr("src","media/mobileintro.mp4");
-
-  }
-
-  $('.intro-continue').click(function() {
-      $('html,body').animate({
-              scrollTop: $('#about').offset().top},
-              'slow');
-  });
-
-  $('.head-d-mid').click(function() {
-    if (document.title == "CMC MUN") {
-      $('html,body').animate({
-              scrollTop: $('#intro').offset().top},
-              'slow');
-            } else {
-                document.location.href = "index.html";
-            }
-  });
-
-  $('#home-mun').click(function() {
-    if (document.title == "CMC MUN") {
-      $('html,body').animate({
-              scrollTop: $('.about-mun').offset().top},
-              'slow');
-            } else {
-                document.location.href = "index.html";
-            }
-  });
-
-  $('#home-us').click(function() {
-    if (document.title == "CMC MUN") {
-
-      $('html,body').animate({
-              scrollTop: $('.about-us').offset().top},
-              'slow');
-            } else {
-                document.location.href = "index.html";
-            }
-  });
+$('.about-direct').click(function() {
+  if (document.title == "CMC MUN") {
+    $('html,body').animate({
+            scrollTop: $('#about').offset().top},
+            'slow');
+          } else {
+              document.location.href = "index.html";
+          }
+});

da6202ff06ef015a526226396f0ff2a79cc0e550
AT - Lab (1 Monitor) <ataha1120@gmail.com>
2019-04-21 18:25:26 -0700

    Navbar mobile
    Foundations for mobile navbar

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
 $("header").load("header.html");
 $("#footer").load("footer.html");
 
+vh = $(window).height()/100;
+vw = $(window).width()/100;
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   $("video").attr("src","media/mobileintro.mp4");
 }
+
+(function ($) {
+  $(document).ready(function(){
+    $(function () {
+        $(window).scroll(function () {
+
+            if ($(this).scrollTop() > (30*vh)) {
+                $('header').fadeIn(300);
+            } else {
+                 $('header').fadeOut(300);
+            }
+        });
+    });
+
+});
+  }(jQuery));
 
 
 $('.intro-continue').click(function() {
